6eaf69e518d886255e4883e6d6bbf1969b24033e
waveq <nynonn@gmail.com>
2014-06-27 15:46:08 +0200

    project in root folder, resources to server

--- konkurs/scripts/buildRedeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "************ UNDEPLOYING *******************"
-asadmin undeploy linkSaver
-echo "************ BUILDING **********************"
-mvn package
-echo "************ DEPLOYING *********************"
-asadmin deploy target/linkSaver.war
